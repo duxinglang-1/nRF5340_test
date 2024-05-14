52af1f3f8557ebb981dc2a3c15fe87798df221f5
xiebiao <biao.x@augustint.com>
2024-05-14 16:23:46 +0800

    保存模拟警报声。

--- PWM_TEST/src/buzzer/buzzer.c
+++ PWM_TEST/src/buzzer/buzzer.c
@@ -49,11 +49,94 @@
 
 struct device *pwm_buzzer1,*pwm_buzzer2,*pwm_buzzer3,*pwm_buzzer4;
 struct pwm_dt_spec sBuzzer1,sBuzzer2,sBuzzer3,sBuzzer4;
+uint8_t count = 0;
+int32_t freq_offset[] = {
+							-1730,-1730,-1730,		//5
+							-1725,-1725,-1725,		//5
+							-1720,-1720,-1720,		//5
+							-1715,-1715,-1715,		//5
+							-1710,-1710,-1710,		//10
+							-1700,-1700,-1700,		//15
+							-1685,-1685,-1685,		//15
+							-1670,-1670,-1670,		//20
+							-1650,-1650,-1650,		//20
+							-1630,-1630,-1630,		//30
+							-1600,-1600,-1600,		//50
+							-1550,-1550,-1550,		//60
+							-1490,-1490,-1490,		//70
+							-1420,-1420,-1420,		//80
+							-1340,-1340,-1340,		//100
+							-1240,-1240,-1240,		//120
+							-1120,-1120,-1120,		//150
+							-970,-970,-970,			//200
+							-770,-770,-770,			//150
+							-620,-620,-620,			//120
+							-500,-500,-500,			//100
+							-400,-400,-400,			//80
+							-320,-320,-320,			//70
+							-250,-250,-250,			//60
+							-190,-190,-190,			//50
+							-140,-140,-140,			//30
+							-110,-110,-110,			//20
+							-90,-90,-90,			//20
+							-70,-70,-70,			//15
+							-55,-55,-55,			//15
+							-40,-40,-40,			//10
+							-30,-30,-30,			//10
+							-20,-20,-20,			//5
+							-15,-15,-15,			//5
+							-10,-10,-10,			//5
+							-5,-5,-5,-5,			//5
+							 0,0,0,0,				//0
+							-5,-5,-5,-5,			//5
+							-10,-10,-10,			//5
+							-15,-15,-15,			//5
+							-20,-20,-20,			//5
+							-30,-30,-30,			//10
+							-40,-40,-40,			//10
+							-55,-55,-55,			//15
+							-70,-70,-70,			//15
+							-90,-90,-90,			//20
+							-110,-110,-110,			//20
+							-140,-140,-140,			//30
+							-190,-190,-190,			//50
+							-250,-250,-250,			//60
+							-320,-320,-320,			//70
+							-400,-400,-400,			//80
+							-500,-500,-500,			//100
+							-620,-620,-620,			//120
+							-770,-770,-770,			//150
+							-970,-970,-970,			//200
+							-1120,-1120,-1120,		//150
+							-1240,-1240,-1240,		//120
+							-1340,-1340,-1340,		//100
+							-1420,-1420,-1420,		//80
+							-1490,-1490,-1490,		//70
+							-1550,-1550,-1550,		//60
+							-1600,-1600,-1600,		//50
+							-1630,-1630,-1630,		//30
+							-1650,-1650,-1650,		//20
+							-1670,-1670,-1670,		//20
+							-1685,-1685,-1685,		//15
+							-1700,-1700,-1700,		//15
+							-1710,-1710,-1710,		//10
+							-1715,-1715,-1715,		//5
+							-1720,-1720,-1720,		//5
+							-1725,-1725,-1725		//5
+							//-1730,-1730,-1730,	    //5
+						};
 
 void buzzer_set(bool OnOff)
 {
+	if(OnOff)
+	{
+		count++;
+		if(count == (sizeof(freq_offset))/(sizeof(freq_offset[0])))
+			count = 0;
+		sBuzzer1.period = PWM_HZ(PWM_FREQ_1+freq_offset[count]);
+	}
 	pwm_set_pulse_dt(&sBuzzer1, OnOff ? (sBuzzer1.period*PWM_DUTY1) : 0);
-	pwm_set_pulse_dt(&sBuzzer2, OnOff ? (sBuzzer2.period*PWM_DUTY2) : 0);
+	//pwm_set_pulse_dt(&sBuzzer2, OnOff ? (sBuzzer2.period*PWM_DUTY2) : 0);
 	//pwm_set_pulse_dt(&sBuzzer3, OnOff ? (sBuzzer3.period*PWM_DUTY3) : 0);
 	//pwm_set_pulse_dt(&sBuzzer4, OnOff ? (sBuzzer4.period*PWM_DUTY4) : 0);
 }
@@ -68,19 +151,19 @@ int buzzer_init(void)
 
 	sBuzzer1.dev = pwm_buzzer1;
 	sBuzzer1.channel = 0;
-	sBuzzer1.period = PWM_HZ(PWM_FREQ_1);
+	sBuzzer1.period = PWM_HZ(PWM_FREQ_1+freq_offset[0]);
 	sBuzzer1.flags = PWM_POLARITY_NORMAL;
 
-	pwm_buzzer2 = DEVICE_DT_GET(BUZZER_2_DEVICE);
-	if(!pwm_buzzer2) 
-	{
-		return;
-	}
+	//pwm_buzzer2 = DEVICE_DT_GET(BUZZER_2_DEVICE);
+	//if(!pwm_buzzer2) 
+	//{
+	//	return;
+	//}
 
-	sBuzzer2.dev = pwm_buzzer2;
-	sBuzzer2.channel = 0;
-	sBuzzer2.period = PWM_HZ(PWM_FREQ_2);
-	sBuzzer2.flags = PWM_POLARITY_NORMAL;
+	//sBuzzer2.dev = pwm_buzzer2;
+	//sBuzzer2.channel = 0;
+	//sBuzzer2.period = PWM_HZ(PWM_FREQ_2);
+	//sBuzzer2.flags = PWM_POLARITY_NORMAL;
 
 	//pwm_buzzer3 = DEVICE_DT_GET(BUZZER_3_DEVICE);
 	//if(!pwm_buzzer3) 
@@ -112,13 +195,13 @@ void BuzzerMsgProcess(void)
 	while(1)
 	{
 		buzzer_set(true);
-		k_sleep(K_MSEC(200));
-		buzzer_set(false);
-		k_sleep(K_MSEC(50));
-		buzzer_set(true);
-		k_sleep(K_MSEC(200));
-		buzzer_set(false);
-		k_sleep(K_MSEC(500));
+		k_sleep(K_MSEC(5));
+		//buzzer_set(false);
+		//k_sleep(K_MSEC(50));
+		//buzzer_set(true);
+		//k_sleep(K_MSEC(200));
+		//buzzer_set(false);
+		//k_sleep(K_MSEC(500));
 	}
 }
 
